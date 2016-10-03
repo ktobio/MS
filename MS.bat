5b90ded9eab41d0361ca4b4b6d57a0b336042b67
ktobio <ktobio@hbs.edu>
2016-10-03 15:00:13 -0400

    Adding complexity
    Separated out the "cleaning" commands from the "analysis" commands, and
    created a .bat (batch) file that runs the two .do files in order.

---
+++ MS.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Stata14\StataMP-64" /b do "C:\Users\ktobio\Desktop\Jeff\MS\MS_cleaning.do"
+"C:\Program Files (x86)\Stata14\StataMP-64" /b do "C:\Users\ktobio\Desktop\Jeff\MS\MS.do"
