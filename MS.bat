707243335c17bf8a0535dcda6f9f7cb807b03fa4
ktobio <ktobio@hbs.edu>
2016-10-14 11:49:42 -0400

    Editing Batch file

--- MS.bat
+++ MS.bat
@@ -1,7 +1,13 @@
+#Nina's computer
 "C:\Program Files (x86)\Stata14\StataMP-64" /b do .\MS_cleaning.do
+#Jeff's computer
+#"C:\Program Files (x86)\Stata14\StataMP-64" /b do .\MS_cleaning.do
 
 del .\MS_cleaning.log
 
+#Nina's computer
 "C:\Program Files (x86)\Stata14\StataMP-64" /b do .\MS.do
+#Jeff's computer
+#"C:\Program Files (x86)\Stata14\StataMP-64" /b do .\MS.do
 
 del .\MS.log
